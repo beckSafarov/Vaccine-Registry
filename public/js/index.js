f0494c1361e3367bc58349c5c6c053a8dd5a01f3
Begzod Safarov <begzod.safarov07@gmail.com>
2021-01-13 13:15:30 +0800

    fixed a bug related to sending email

--- public/js/index.js
+++ public/js/index.js
@@ -4,35 +4,31 @@ const email = document.getElementById('email');
 const message = document.getElementById('textarea');
 const warningBtn = document.getElementById('warning-btn');
 const spinner = document.getElementById('spinner');
-root = location.host; 
 
-
-
-const sidenav = document.querySelector('.sidenav');
-M.Sidenav.init(sidenav, {});
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.slider');
-    var instances = M.Slider.init(elems, {
+    var sliderElems = document.querySelectorAll('.slider');
+    M.Slider.init(sliderElems, {
         indicators: false,
         height: 780,
         transition: 500,
         interval: 4000
     });
+
+    var dropdownElems = document.querySelector('.dropdown-trigger');
+    M.Dropdown.init(dropdownElems, {});
+
+    var scrollspyElems = document.querySelectorAll('.scrollspy');
+    M.ScrollSpy.init(scrollspyElems, {});
+
+    const sidenav = document.querySelector('.sidenav');
+    M.Sidenav.init(sidenav, {});    
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelector('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, {});
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.scrollspy');
-    var instances = M.ScrollSpy.init(elems, {});
-});
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', async function(e){
     e.preventDefault();
     spinner.classList.add('active');
     const data = {
@@ -40,22 +36,43 @@ form.addEventListener('submit', function(e){
         email: email.value,
         message: message.value
     }
-
-    post(`${root}/email`, data)
-    .then(data => {
-        spinner.classList.remove('active');
-        fullName.value = '';
-        email.value = '';
-        message.value = '';
-        if(data.success == false){
-            warningBtn.innerHTML = data.msg;
-            warningBtn.style.color = 'red';
-        }else{
-            warningBtn.innerHTML = data.msg;
-            warningBtn.style.color = 'green';
-        }
+    
+    const response = await fetch(`${root}/email`, {
+        method: 'POST',
+        headers: {
+                'Content-type':'application/json'
+        },
+        body: JSON.stringify(data)
     })
-    .catch(err => console.log(err));
+    
+    const resData = await response.json();
+    spinner.classList.remove('active');
+    fullName.value = '';
+    email.value = '';
+    message.value = '';
+    console.log(resData);
+    if(resData.success === false){
+        warningBtn.innerHTML = resData.msg;
+        warningBtn.style.color = 'red';
+    }else{
+        warningBtn.innerHTML = resData.msg;
+        warningBtn.style.color = 'green';
+    }
+    // post(`${root}/email`, data)
+    // .then(data => {
+    //     spinner.classList.remove('active');
+    //     fullName.value = '';
+    //     email.value = '';
+    //     message.value = '';
+    //     if(data.success == false){
+    //         warningBtn.innerHTML = data.msg;
+    //         warningBtn.style.color = 'red';
+    //     }else{
+    //         warningBtn.innerHTML = data.msg;
+    //         warningBtn.style.color = 'green';
+    //     }
+    // })
+    // .catch(err => console.log(err));
 
 }) //end of the form
 
